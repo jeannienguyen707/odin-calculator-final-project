9bb44f00919124eb44dba484a5652014a22db8fd
Jeannie Nguyen <Jeannienguyen888@gmail.com>
2026-09-07 12:01:33 -0700

    Make outline for implemention
    Comment how code should function to better
    understand how every piece will work, what could
    be missing or what should be done better

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,2 +1,38 @@
 const base = document.querySelector(".calBody")
 const display = document.createElement("div")
+display.classList.add("showNums")
+base.add(display)
+
+let score = 0
+
+// Numbers
+for (let i = 0; i < 10; i++) {
+    let newNum = document.createElement("div")
+    newNum.addEventListener("click", )
+
+}
+// Operations
+for (let i = 0; i < 4; i++) {
+    
+}
+// equal and clear button
+
+
+
+//return value function
+// and checks value function
+
+
+// check values function
+
+/*
+operations only?  just change op
+1 num value
+operations?
+
+if  /0 vs /10
+
+store as a string and break it down? no ne
+
+
+*/
